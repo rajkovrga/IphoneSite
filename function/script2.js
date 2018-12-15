849cafa4380a82c5003ba9451cb589277d14e9f5
Rajko Vrga <rajkovrga@yahoo.com>
2018-12-15 20:41:51 +0100

    add new articles

---
+++ function/script2.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded',function()
+{
+   
+    var ajaxLoad = new XMLHttpRequest();
+   
+    ajaxLoad.open('POST','products.json');
+    
+    
+
+  
+
+});    
